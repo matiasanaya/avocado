a9e8e52941f7ba357097151e5d1b5c0c7b3eaa03
Matias Anaya <matiasanaya@gmail.com>
2013-05-10 11:08:58 -0700

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avocado::Application.config.secret_token = 'c77abb97bc9f55e537212ae1869b6162880c441ebd2c814d9a8c03b739bb822a78927baca873d18c7780f1928d546e7dfcc42313ba59acee380e3273a92dce0a'
