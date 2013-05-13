fa1eb954e9c564eb95977f1e4425ec5add19181c
Matias Anaya <matiasanaya@gmail.com>
2013-05-12 17:33:44 -0700

    Implements mail intercepter

--- config/environment.rb
+++ config/environment.rb
@@ -29,3 +29,5 @@ ActionMailer::Base.smtp_settings = {
 }
 
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+require 'development_mail_interceptor'
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
