33cecd9ef4fba8adff2ddc0ca86a08a1db4dbbef
Matias Anaya <matiasanaya@gmail.com>
2013-05-15 17:07:45 -0700

    Validates email in email_shares

--- app/models/email_share.rb
+++ app/models/email_share.rb
@@ -4,7 +4,8 @@ class EmailShare < ActiveRecord::Base
 
   belongs_to :card
 
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :card, presence: true
 
   def update
